c0d6aa6faa8c48b847afd11963307961f3e30102
Joy-Shin <joy.shin.2213@gmail.com>
2018-02-24 12:49:24 -0500

    initial commit

---
+++ Scripts/objects/plane.ts
@@ -0,0 +1,51 @@
+module objects {
+  export class Plane extends objects.GameObject {
+    // Private Instance Variables
+
+
+    // Public Properties
+
+
+    // Constructors
+    constructor(assetManager: createjs.LoadQueue) {
+      super(assetManager, "plane");
+      this.Start();
+    }
+
+    // Private Methods
+
+    // Public Methods
+
+    // Initialization
+    public Reset():void {
+
+    }
+
+    public CheckBounds():void {
+      // check the right boundary
+      if(this.x >= 640 - this.halfWidth) {
+        this.x = 640 - this.halfWidth;
+      }
+
+      // check the left boundary
+      if(this.x <= this.halfWidth) {
+        this.x = this.halfWidth;
+      }
+    }
+
+    public Move():void {
+      this.x = objects.Game.stage.mouseX;
+    }
+
+    public Start():void {
+      this.y = 430;
+    }
+
+    // Updates the Object every frame
+    public Update():void {
+      this.Move();
+      this.CheckBounds();
+    }
+
+  }
+}
---
+++ Scripts/scenes/play.ts
@@ -0,0 +1,72 @@
+module scenes {
+  export class PlayScene extends objects.Scene {
+    // Private Instance Variables
+    private _ocean: objects.Ocean;
+    private _plane: objects.Plane;
+    private _island: objects.Island;
+    private _clouds: objects.Cloud[];
+    private _cloudNum: number;
+
+    // Public Properties
+
+    // Constructor
+    constructor(assetManager: createjs.LoadQueue) {
+      super(assetManager);
+
+      this.Start();
+    }
+
+    // Private Mathods
+
+
+
+    // Public Methods
+
+    // Initialize Game Variables and objects
+    public Start(): void {
+      this._cloudNum = 3;
+      this._ocean = new objects.Ocean(this.assetManager);
+      this._plane = new objects.Plane(this.assetManager);
+      this._island = new objects.Island(this.assetManager);
+
+      // create the cloud array
+      this._clouds = new Array<objects.Cloud>();
+
+      // add clouds to the array
+      for (let count = 0; count < this._cloudNum; count++) {
+         this._clouds[count] =  new objects.Cloud(this.assetManager);
+      }
+
+      this.Main();
+    }
+
+    public Update(): void {
+      this._ocean.Update();
+      this._plane.Update();
+      this._island.Update();
+
+      // update each cloud
+      this._clouds.forEach(cloud => {
+        cloud.Update();
+      });
+    }
+
+    // This is where the fun happens
+    public Main(): void {
+      // add ocean to the scene
+      this.addChild(this._ocean);
+
+      // add island to this scene
+      this.addChild(this._island);
+
+      // add plane to this scene
+      this.addChild(this._plane);
+
+      // add clouds to the scene
+      this._clouds.forEach(cloud => {
+        this.addChild(cloud);
+      });
+
+    }
+  }
+}
